292282ff4ebedb8e6c170c9e0843156a4108459d
Edwin Sanchez <edwinalex035@gmail.com>
2020-04-04 17:14:04 -0600

    :tada: Initial commit

---
+++ src/main/java/com/ed/storeparent/StoreParentApplication.java
@@ -0,0 +1,13 @@
+package com.ed.storeparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreParentApplication.class, args);
+	}
+
+}
